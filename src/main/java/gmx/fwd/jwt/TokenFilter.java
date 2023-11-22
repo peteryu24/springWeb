3af96fb6a75442c1481fbe0a1ea70ea5dec875bb
DudeFromKorea <peteryu24@naver.com>
2023-11-22 11:49:24 +0900

    exception in token filter

--- src/main/java/gmx/fwd/jwt/TokenFilter.java
+++ src/main/java/gmx/fwd/jwt/TokenFilter.java
@@ -6,11 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
-import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 
 public class TokenFilter extends GenericFilterBean {
 
@@ -19,29 +20,34 @@ public class TokenFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("doFilter in TokenFilter is called");
-        String token = resolveToken((HttpServletRequest) req); // http 요청으로 토큰 추출
-        System.out.println("token is " + token);
+        HttpServletRequest httpRequest = (HttpServletRequest) req;
+        HttpServletResponse httpResponse = (HttpServletResponse) res;
+
         try {
+        	
+            String token = resolveToken(httpRequest);
+            
             if (token != null && tokenProvider.validateToken(token)) {
-                // 유효성 검증
                 Authentication auth = tokenProvider.getAuthentication(token);
-                System.out.println("Authentication: " + auth); // 추가 로깅
-                // 인증 정보 설정
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
-        } catch (ExpiredJwtException e) {
-            System.out.println("Token Expired: " + e.getMessage()); // 토큰 만료 로그
+            
+            filterChain.doFilter(req, res);
+            
+        } catch (JwtException e) {
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 상태 코드 설정
+            httpResponse.getWriter().write("Unauthorized: " + e.getMessage());
+            return; // 필터 체인의 나머지 부분을 실행하지 않고 요청 처리 중단
         } catch (Exception e) {
-            System.out.println("Token Validation Error: " + e.getMessage()); // 예외 처리 로그
+            httpResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // 500 상태 코드 설정
+            httpResponse.getWriter().write("Internal Server Error: " + e.getMessage());
+            return; // 마찬가지로 처리 중단
         }
-        filterChain.doFilter(req, res);
     }
 
     private String resolveToken(HttpServletRequest req) {
         String bearerToken = req.getHeader("Authorization");
-        System.out.println("bearerToken is: " + bearerToken);
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) { // bearer 토큰 추출
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
         return null;
