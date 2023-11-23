b2cff9d3c47f328f96d58239e1553e0cf2c60edf
DudeFromKorea <peteryu24@naver.com>
2023-11-23 11:16:57 +0900

    create a controller for checking token's validity

---
+++ src/main/java/gmx/fwd/jwt/TokenVerifier.java
@@ -0,0 +1,34 @@
+package gmx.fwd.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class TokenVerifier {
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @GetMapping("/verifyToken.do")
+    public ResponseEntity<?> verifyToken(HttpServletRequest request) {
+
+        String token = request.getHeader("Authorization");
+        
+        if (token == null || !token.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
+
+        // 토큰 검증
+        boolean isValid = tokenProvider.validateToken(token.substring(7));
+        
+        if (!isValid) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
+
+        return ResponseEntity.ok("Token is valid");
+    }
+}
