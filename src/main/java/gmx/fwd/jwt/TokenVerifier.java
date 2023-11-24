770908a51c85a77349cbd56392a8cd88d2475004
DudeFromKorea <peteryu24@naver.com>
2023-11-24 09:05:53 +0900

    @RequestMapping("/token") added
    url added

--- src/main/java/gmx/fwd/jwt/TokenVerifier.java
+++ src/main/java/gmx/fwd/jwt/TokenVerifier.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@RequestMapping("/token")
 public class TokenVerifier {
 
     @Autowired
