e83ed1675e1d824f1d1b649c17fcdd3e3782978e
DudeFromKorea <peteryu24@naver.com>
2023-11-24 13:37:13 +0900

    added refreshToken.do

--- src/main/java/gmx/fwd/jwt/TokenVerifier.java
+++ src/main/java/gmx/fwd/jwt/TokenVerifier.java
@@ -3,9 +3,16 @@ package gmx.fwd.jwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/token")
@@ -32,4 +39,33 @@ public class TokenVerifier {
 
         return ResponseEntity.ok("Token is valid");
     }
+    
+    @GetMapping("/refreshToken.do")
+    public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        Cookie[] cookies = request.getCookies();
+        String refreshToken = null;
+        for (Cookie cookie : cookies) {
+            if ("refreshToken".equals(cookie.getName())) {
+                refreshToken = cookie.getValue();
+                break;
+            }
+        }
+
+        if (refreshToken == null || !tokenProvider.validateToken(refreshToken)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or expired refresh token");
+        }
+        
+        String username = tokenProvider.getUsernameFromToken(refreshToken);
+        
+        String role = authentication.getAuthorities().stream()
+                .findFirst()
+                .map(grantedAuthority -> grantedAuthority.getAuthority())
+                .orElse("ROLE_USER");
+        String newAccessToken = tokenProvider.createRefreshToken(username, role);
+
+        return ResponseEntity.ok(Collections.singletonMap("token", newAccessToken));
+    }
+
+
+
 }
