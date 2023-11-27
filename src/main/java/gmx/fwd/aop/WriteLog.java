3ce8fe1fc9c2eff0a77b8c5726c69feca73dd42c
DudeFromKorea <peteryu24@naver.com>
2023-11-27 10:57:39 +0900

    changed pointcut for login log

--- src/main/java/gmx/fwd/aop/WriteLog.java
+++ src/main/java/gmx/fwd/aop/WriteLog.java
@@ -3,7 +3,7 @@ package gmx.fwd.aop;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing; 
+import org.aspectj.lang.annotation.AfterThrowing;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -14,6 +14,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import gmx.fwd.mapper.log.LogMapper;
@@ -28,6 +29,10 @@ public class WriteLog {
 
 	private static final Logger logger = LoggerFactory.getLogger(WriteLog.class);
 
+	@Pointcut("execution(* gmx.fwd.jwt.MyAuthenticationSuccessHandler.onAuthenticationSuccess(..))")
+	private void onAuthenticationSuccess() {
+	}
+
 	/*
 	 * 로깅을 설정할 클래스의 메소드 정의	 
 	* PostService의 writePost
@@ -36,8 +41,42 @@ public class WriteLog {
 	private void forWritePost() {
 	}
 
-	@Pointcut("execution(* gmx.fwd.security.MyUserDetailService.loadUserByUsername(String))")
-	private void forSecurityLogin() {
+	@AfterReturning("onAuthenticationSuccess()")
+	public void afterOnAuthenticationSuccess(JoinPoint joinPoint) {
+		final Object[] loginInfoArr = joinPoint.getArgs();
+		int loginInfoLength = loginInfoArr.length;
+
+		Authentication authentication = null;
+
+		// Authentication 타입 위치 찾기
+		for (int i = 0; i < loginInfoLength; i++) {
+			if (loginInfoArr[i] instanceof Authentication) {
+				authentication = (Authentication) loginInfoArr[i];
+				break;
+			}
+		}
+
+		if (authentication != null) {
+			String username = authentication.getName();
+			logger.info("Login 시도 at: " + joinPoint.getSignature().getName());
+			logger.info("Login successful for user: " + username);
+
+			LogVO loginLog = new LogVO();
+			loginLog.setCategory(1);
+			loginLog.setUsername(username);
+			loginLog.setActivity(username + " logged in");
+
+			if (!(logMapper.writeLog(loginLog) > 0)) {
+				logger.info(username + "의 로그인 로그 DB업로드 실패");
+				return;
+			}
+
+			if ((logMapper.getLogCount() > 50)) {
+				logMapper.deleteOldestLog();
+			}
+
+			logger.info(username + "의 로그인 로그 DB업로드 성공");
+		}
 	}
 
 	// writePost 메소드 실행 전
@@ -45,33 +84,6 @@ public class WriteLog {
 	public void beforeWritePost(JoinPoint joinPoint) {
 
 		logger.info("writePost 시도: " + joinPoint.getSignature().getName());
-	}
-
-	// writePost 메소드 실행 후 에러가 발생하지 않는다면
-	@AfterReturning("forWritePost()")
-	public void noErrorInWritePost(JoinPoint joinPoint) {
-		final Object[] writePostInfoArr = joinPoint.getArgs();
-		final List<Object> writePostInfoList = Collections.unmodifiableList(Arrays.asList(writePostInfoArr));
-		LogVO writePostLog = new LogVO();
-
-		logger.info("writePost 성공: " + joinPoint.getSignature().getName());
-		logger.info("아이디: " + writePostInfoList.get(0));
-		logger.info("제목: " + writePostInfoList.get(1));
-
-		writePostLog.setCategory(2);
-		writePostLog.setUsername((String) writePostInfoList.get(0));
-		writePostLog.setActivity("Post \"" + (String) writePostInfoList.get(1) + "\" Written");
-
-		if (!(logMapper.writeLog(writePostLog) > 0)) {
-			logger.info(writePostInfoList.get(0) + "의 게시글 작성 로그 DB업로드 실패");
-			return;
-		}
-
-		if ((logMapper.getLogCount() > 50)) {
-			logMapper.deleteOldestLog();
-		}
-
-		logger.info(writePostInfoList.get(0) + "의 게시글 작성 로그 DB업로드 성공");
 	}
 
 	// writePost의 transaction에서 에러가 발생할 경우
@@ -99,22 +111,23 @@ public class WriteLog {
 		logger.info(writePostInfoList.get(0) + "의 게시글 작성 실패 로그 DB업로드 성공");
 	}
 
-	@AfterReturning("forSecurityLogin()")
-	public void loginSuccessful(JoinPoint joinPoint) {
-		final Object[] loginInfoArr = joinPoint.getArgs();
-		final List<Object> loginInfoList = Collections.unmodifiableList(Arrays.asList(loginInfoArr));
+	// writePost 메소드 실행 후 에러가 발생하지 않는다면
+	@AfterReturning("forWritePost()")
+	public void noErrorInWritePost(JoinPoint joinPoint) {
+		final Object[] writePostInfoArr = joinPoint.getArgs();
+		final List<Object> writePostInfoList = Collections.unmodifiableList(Arrays.asList(writePostInfoArr));
+		LogVO writePostLog = new LogVO();
 
-		LogVO loginLog = new LogVO();
+		logger.info("writePost 성공: " + joinPoint.getSignature().getName());
+		logger.info("아이디: " + writePostInfoList.get(0));
+		logger.info("제목: " + writePostInfoList.get(1));
 
-		logger.info("Login 시도 at: " + joinPoint.getSignature().getName());
-		logger.info("아이디: " + loginInfoList.get(0));
+		writePostLog.setCategory(2);
+		writePostLog.setUsername((String) writePostInfoList.get(0));
+		writePostLog.setActivity("Post \"" + (String) writePostInfoList.get(1) + "\" Written");
 
-		loginLog.setCategory(1);
-		loginLog.setUsername((String) loginInfoList.get(0));
-		loginLog.setActivity((String) loginInfoList.get(0) + " logged in");
-
-		if (!(logMapper.writeLog(loginLog) > 0)) {
-			logger.info(loginInfoList.get(0) + "의 로그인 로그 DB업로드 실패");
+		if (!(logMapper.writeLog(writePostLog) > 0)) {
+			logger.info(writePostInfoList.get(0) + "의 게시글 작성 로그 DB업로드 실패");
 			return;
 		}
 
@@ -122,7 +135,7 @@ public class WriteLog {
 			logMapper.deleteOldestLog();
 		}
 
-		logger.info(loginInfoList.get(0) + "의 로그인 로그 DB업로드 성공");
+		logger.info(writePostInfoList.get(0) + "의 게시글 작성 로그 DB업로드 성공");
 	}
 
 }
